13b45bdb3e2e643dda31a8de2b9b2ddc72699bec
abdallah.sarayrah <abdallah.dev85@gmail.com>
2018-10-17 10:34:17 +0300

    Initial commit

---
+++ app/src/main/java/com/sarayrah/abdallah/eventsnotify/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.sarayrah.abdallah.eventsnotify
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
