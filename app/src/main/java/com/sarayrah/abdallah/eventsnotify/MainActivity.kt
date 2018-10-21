ca1d0865800f715b052cda42d6e4b3b55cc2597a
abdallah.sarayrah <abdallah.dev85@gmail.com>
2018-10-21 14:07:04 +0300

    delete the log.d line from the main activity

--- app/src/main/java/com/sarayrah/abdallah/eventsnotify/MainActivity.kt
+++ app/src/main/java/com/sarayrah/abdallah/eventsnotify/MainActivity.kt
@@ -3,7 +3,13 @@ package com.sarayrah.abdallah.eventsnotify
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.google.android.gms.common.GoogleApiAvailability
 import com.google.firebase.iid.FirebaseInstanceId
+import android.widget.Toast
+import android.support.annotation.NonNull
+import com.google.android.gms.tasks.OnFailureListener
+import com.google.android.gms.tasks.OnSuccessListener
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,6 +17,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Log.d("fcm", FirebaseInstanceId.getInstance().token)
+        GoogleApiAvailability.getInstance()
+                .makeGooglePlayServicesAvailable(this)
+                .addOnSuccessListener {
+                    Log.d("fcm", "makeGooglePlayServicesAvailable().onSuccess()")
+
+                    // GPS available; do something useful
+                }.addOnFailureListener(this) { e ->
+                    Log.d("fcm", "makeGooglePlayServicesAvailable().onFailure()")
+                    e.printStackTrace()
+
+                    // can't proceed without GPS; quit
+
+                    this@MainActivity.finish() // this causes a crash
+                }
     }
 }
