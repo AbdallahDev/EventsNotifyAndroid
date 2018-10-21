91ece5d58c2bb91325800e7905639ff2a84e8741
abdallah.sarayrah <abdallah.dev85@gmail.com>
2018-10-21 13:29:49 +0300

    adding the FirebaseMessagingService to receive messages.

---
+++ app/src/main/java/com/sarayrah/abdallah/eventsnotify/services/MyFirebaseMessagingService.kt
@@ -0,0 +1,14 @@
+package com.sarayrah.abdallah.eventsnotify.services
+
+import android.util.Log
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+    override fun onMessageReceived(p0: RemoteMessage?) {
+        super.onMessageReceived(p0)
+
+        Log.d("fcm_msg", p0?.notification?.body)
+        Log.d("fcm_msg", p0?.notification?.title)
+    }
+}
