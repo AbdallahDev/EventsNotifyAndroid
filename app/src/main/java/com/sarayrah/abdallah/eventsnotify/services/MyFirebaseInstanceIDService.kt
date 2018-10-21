3bde318622309df4bdd5f0f3b7c31cd48448e62e
abdallah.sarayrah <abdallah.dev85@gmail.com>
2018-10-21 12:46:17 +0300

    adding the service to get the device token for the fcm.

---
+++ app/src/main/java/com/sarayrah/abdallah/eventsnotify/services/MyFirebaseInstanceIDService.kt
@@ -0,0 +1,14 @@
+package com.sarayrah.abdallah.eventsnotify.services
+
+import android.util.Log
+import com.google.firebase.iid.FirebaseInstanceId
+import com.google.firebase.iid.FirebaseInstanceIdService
+
+class MyFirebaseInstanceIDService : FirebaseInstanceIdService() {
+    override fun onTokenRefresh() {
+        super.onTokenRefresh()
+
+        val refreshedToken = FirebaseInstanceId.getInstance().token
+        Log.d("fcm_toke", refreshedToken)
+    }
+}
