d5c78eff3311f281446be6dfcd58007a589dd021
abdallah.sarayrah <abdallah.dev85@gmail.com>
2018-10-25 13:28:39 +0300

    make the notification builder

--- app/src/main/java/com/sarayrah/abdallah/eventsnotify/services/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/sarayrah/abdallah/eventsnotify/services/MyFirebaseMessagingService.kt
@@ -1,11 +1,16 @@
 package com.sarayrah.abdallah.eventsnotify.services
 
+import android.app.Notification
 import android.app.PendingIntent
 import android.content.Intent
+import android.support.v4.app.NotificationCompat
+import android.support.v4.app.NotificationManagerCompat
 import android.util.Log
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 import com.sarayrah.abdallah.eventsnotify.EventDetails
+import com.sarayrah.abdallah.eventsnotify.R
+import java.util.*
 
 class MyFirebaseMessagingService : FirebaseMessagingService() {
     override fun onMessageReceived(p0: RemoteMessage?) {
@@ -23,5 +28,25 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
         val notifyPendingIntent = PendingIntent.getActivity(
                 this, 0, notifyIntent, PendingIntent.FLAG_UPDATE_CURRENT
         )
+
+        //make the notification builder
+        val mBuilder = NotificationCompat.Builder(this,
+                resources.getString(R.string.default_notification_channel_id))
+                .setSmallIcon(R.drawable.ic_stat_name)
+                .setContentTitle(p0?.data?.get("title"))
+                .setContentText(p0?.data?.get("body"))
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setStyle(NotificationCompat.BigTextStyle()
+                        .bigText(p0?.data?.get("body")))
+                .setDefaults(Notification.DEFAULT_ALL)
+                // Set the intent that will fire when the user taps the notification
+                .setContentIntent(notifyPendingIntent)
+                .setAutoCancel(true)
+        with(NotificationManagerCompat.from(this)) {
+            //create random notification id
+            val notificationId = Random().nextInt()
+            // notificationId is a unique int for each notification that you must define
+            notify(notificationId, mBuilder.build())
+        }
     }
 }
