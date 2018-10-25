63d22f3cf14da9da905645218ff23bf585fab555
abdallah.sarayrah <abdallah.dev85@gmail.com>
2018-10-25 13:18:26 +0300

    make the create notification function

--- app/src/main/java/com/sarayrah/abdallah/eventsnotify/MainActivity.kt
+++ app/src/main/java/com/sarayrah/abdallah/eventsnotify/MainActivity.kt
@@ -1,5 +1,9 @@
 package com.sarayrah.abdallah.eventsnotify
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -32,6 +36,26 @@ class MainActivity : AppCompatActivity() {
     private fun googlePlayServicesAvailable() {
         GoogleApiAvailability.getInstance()
                 .makeGooglePlayServicesAvailable(this)
+    }
+
+    //this fun to create notification channel on android oreo and higher.
+    private fun createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = getString(R.string.channel_name)
+            val descriptionText = getString(R.string.channel_description)
+            val importance = NotificationManager.IMPORTANCE_HIGH
+            val channel = NotificationChannel(
+                    resources.getString(R.string.default_notification_channel_id), name, importance)
+                    .apply {
+                        description = descriptionText
+                    }
+            // Register the channel with the system
+            val notificationManager: NotificationManager =
+                    getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
     }
 
     //this fun to pint device instanceID in the LOG
