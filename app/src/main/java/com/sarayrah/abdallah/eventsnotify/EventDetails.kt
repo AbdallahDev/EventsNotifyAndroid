650d84955cc039f99275f4fb93aa2f2004680e7f
abdallah.sarayrah <abdallah.dev85@gmail.com>
2018-10-25 13:08:58 +0300

    add the event details activity and change its manifest details

---
+++ app/src/main/java/com/sarayrah/abdallah/eventsnotify/EventDetails.kt
@@ -0,0 +1,12 @@
+package com.sarayrah.abdallah.eventsnotify
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class EventDetails : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_event_details)
+    }
+}
