80fa8247c1359876ca16f14f3ed836e86ab55210
abdallah.sarayrah <abdallah.dev85@gmail.com>
2018-11-01 15:49:37 +0200

    serverIp variable

--- app/src/main/java/com/sarayrah/abdallah/eventsnotify/data/Data.kt
+++ app/src/main/java/com/sarayrah/abdallah/eventsnotify/data/Data.kt
@@ -4,8 +4,10 @@ class Data {
 
     companion object {
         /*bellow is the server ip, it will be changed every time while i'm working on the app
-         development, coz i work locally and on a real server*/
-        val serverIp = "10.153.70.145"
+         development, coz i work locally and on a real server
+         local ip: 10.153.70.145
+         real server ip: 193.188.88.148*/
+        val serverIp = "193.188.88.148"
         //bellow are variables to store url, so i don't need to edit it everywhere in the code
         //this the url for the base web system
         var rootUrl = "http://${Data.serverIp}/apps/android/MyApps/eventsNotify/"
