8d9948b142ba430eca706d639b35a6a9eba8acb2
abdallah.sarayrah <abdallah.dev85@gmail.com>
2018-11-01 14:06:47 +0200

    add the needed data to view them in the recyclerView

--- app/src/main/java/com/sarayrah/abdallah/eventsnotify/recyclerView/EventsDataSet.kt
+++ app/src/main/java/com/sarayrah/abdallah/eventsnotify/recyclerView/EventsDataSet.kt
@@ -1,4 +1,4 @@
 package com.sarayrah.abdallah.eventsnotify.recyclerView
 
-class EventsDataSet(var eventTitle: String) {
-}
+class EventsDataSet(var eventTitle: String, var eventSubject: String, var eventDate: String,
+                    var eventTime: String)
