22ec702713b89deb7c145c705c143ab20c4af981
abdallah.sarayrah <abdallah.dev85@gmail.com>
2018-11-01 14:02:15 +0200

    here i've completed save the device token to db

--- app/src/main/java/com/sarayrah/abdallah/eventsnotify/services/MyFirebaseInstanceIDService.kt
+++ app/src/main/java/com/sarayrah/abdallah/eventsnotify/services/MyFirebaseInstanceIDService.kt
@@ -1,12 +1,37 @@
 package com.sarayrah.abdallah.eventsnotify.services
 
+import android.util.Log
+import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.google.firebase.iid.FirebaseInstanceId
 import com.google.firebase.iid.FirebaseInstanceIdService
+import com.sarayrah.abdallah.eventsnotify.data.Data
 
 class MyFirebaseInstanceIDService : FirebaseInstanceIdService() {
     override fun onTokenRefresh() {
         super.onTokenRefresh()
 
         val refreshedToken = FirebaseInstanceId.getInstance().token
+
+        //call this fun to store device token to the server.
+        sendRegistrationToServer(refreshedToken)
+    }
+
+    /*this fun sends device token to the server to store it in the db so the device can receive
+    notifications*/
+    private fun sendRegistrationToServer(refreshedToken: String?) {
+        val url = "${Data.saveDeviceTokenUrl}$refreshedToken"
+        val requestQueue = Volley.newRequestQueue(this)
+        val stringQueue = StringRequest(Request.Method.GET, url, Response.Listener {
+            Toast.makeText(this, "تم تسجيلك لتلقي تنبيهات نشاطات المجلس",
+                    Toast.LENGTH_LONG).show()
+            Log.wtf("fcm", "DeviceTokenSaved = $refreshedToken")
+        }, Response.ErrorListener { error ->
+            Toast.makeText(this, error.message, Toast.LENGTH_SHORT).show()
+        })
+        requestQueue.add(stringQueue)
     }
 }
