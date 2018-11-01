b3bba25a49ec2609b44fb385fb520bf25cae9ba7
abdallah.sarayrah <abdallah.dev85@gmail.com>
2018-11-01 14:54:55 +0200

    this code to get the events from the db and view it in the app

--- app/src/main/java/com/sarayrah/abdallah/eventsnotify/recyclerView/EventsAdapter.kt
+++ app/src/main/java/com/sarayrah/abdallah/eventsnotify/recyclerView/EventsAdapter.kt
@@ -22,6 +22,9 @@ class EventsAdapter(private val list: ArrayList<EventsDataSet>) :
 
     override fun onBindViewHolder(p0: ViewHolder, p1: Int) {
         p0.itemView.tv_title.text = list[p1].eventTitle
+        p0.itemView.tv_title.text = list[p1].eventTitle
+        p0.itemView.tv_title.text = list[p1].eventTitle
+        p0.itemView.tv_title.text = list[p1].eventTitle
     }
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
