4cf6858bc191c79b8e7914bcd43ecba0733a3ca6
abdallah.sarayrah <abdallah.dev85@gmail.com>
2018-11-04 13:48:23 +0200

    add the baseUrl variable coz i've migrated the android php code to the original events php project so the website and the android app work cna be centralized.

--- app/src/main/java/com/sarayrah/abdallah/eventsnotify/data/Data.kt
+++ app/src/main/java/com/sarayrah/abdallah/eventsnotify/data/Data.kt
@@ -10,10 +10,10 @@ class Data {
         val serverIp = "193.188.88.148"
         //bellow are variables to store url, so i don't need to edit it everywhere in the code
         //this the url for the base web system
-        var rootUrl = "http://${Data.serverIp}/apps/android/MyApps/eventsNotify/"
+        var baseUrl = "http://${Data.serverIp}/apps/android/MyApps/eventsNotify"
         //this is the url for saveDeviceToken api
-        var saveDeviceTokenUrl = "http://${Data.serverIp}/apps/android/MyApps/eventsNotify/android/apis/saveDeviceToken.php?deviceToken="
+        var saveDeviceTokenUrl = "$baseUrl/android/apis/saveDeviceToken.php?deviceToken="
         //events fetching api url
-        var getEventsUrl = "http://${Data.serverIp}/apps/android/MyApps/eventsNotify/android/apis/getEvents.php"
+        var getEventsUrl = "$baseUrl/android/apis/getEvents.php"
     }
 }
