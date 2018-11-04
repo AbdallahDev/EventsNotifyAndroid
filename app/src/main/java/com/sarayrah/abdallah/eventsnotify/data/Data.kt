e89148d37dcca862814e02e22892e2d24cf0bdf3
abdallah.sarayrah <abdallah.dev85@gmail.com>
2018-11-04 13:51:21 +0200

    updated the base url

--- app/src/main/java/com/sarayrah/abdallah/eventsnotify/data/Data.kt
+++ app/src/main/java/com/sarayrah/abdallah/eventsnotify/data/Data.kt
@@ -10,7 +10,7 @@ class Data {
         val serverIp = "193.188.88.148"
         //bellow are variables to store url, so i don't need to edit it everywhere in the code
         //this the url for the base web system
-        var baseUrl = "http://${Data.serverIp}/apps/android/MyApps/eventsNotify"
+        var baseUrl = "http://${Data.serverIp}/apps/MyApps/events"
         //this is the url for saveDeviceToken api
         var saveDeviceTokenUrl = "$baseUrl/android/apis/saveDeviceToken.php?deviceToken="
         //events fetching api url
