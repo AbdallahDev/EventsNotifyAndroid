644f62d55c73e908c55a5467c8140d7a3a3b51a9
abdallah.sarayrah <abdallah.dev85@gmail.com>
2018-11-07 13:20:40 +0200

    get data as html in the EventsDetails Activity coz it contains html tags

--- app/src/main/java/com/sarayrah/abdallah/eventsnotify/EventDetails.kt
+++ app/src/main/java/com/sarayrah/abdallah/eventsnotify/EventDetails.kt
@@ -3,6 +3,7 @@ package com.sarayrah.abdallah.eventsnotify
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Html
 import kotlinx.android.synthetic.main.activity_event_details.*
 
 class EventDetails : AppCompatActivity() {
@@ -11,10 +12,10 @@ class EventDetails : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_event_details)
 
-        tv_title.text = intent.getStringExtra("title")
-        tv_body.text = intent.getStringExtra("body")
-        tv_date.text = intent.getStringExtra("date")
-        tv_time.text = intent.getStringExtra("time")
+        tv_title.text = Html.fromHtml(intent.getStringExtra("title"))
+        tv_body.text = Html.fromHtml(intent.getStringExtra("body"))
+        tv_date.text = Html.fromHtml(intent.getStringExtra("date"))
+        tv_time.text = Html.fromHtml(intent.getStringExtra("time"))
 
         //by this button i'll navigate to the activity that view all the events.
         btn_all_events.setOnClickListener {
