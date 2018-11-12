d87c9561934e4c8b1272b60fac8bacc15c7942b2
abdallah.sarayrah <abdallah.dev85@gmail.com>
2018-11-12 11:31:15 +0200

    made the screen wake up code

--- app/src/main/java/com/sarayrah/abdallah/eventsnotify/MainActivity.kt
+++ app/src/main/java/com/sarayrah/abdallah/eventsnotify/MainActivity.kt
@@ -7,8 +7,7 @@ import android.os.Build
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
-import android.util.Log
-import android.util.Log.*
+import android.util.Log.d
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
--- app/src/main/java/com/sarayrah/abdallah/eventsnotify/services/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/sarayrah/abdallah/eventsnotify/services/MyFirebaseMessagingService.kt
@@ -1,8 +1,11 @@
 package com.sarayrah.abdallah.eventsnotify.services
 
+import android.annotation.SuppressLint
 import android.app.Notification
 import android.app.PendingIntent
+import android.content.Context
 import android.content.Intent
+import android.os.PowerManager
 import android.support.v4.app.NotificationCompat
 import android.support.v4.app.NotificationManagerCompat
 import com.google.firebase.messaging.FirebaseMessagingService
@@ -15,13 +18,22 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
     override fun onMessageReceived(p0: RemoteMessage?) {
         super.onMessageReceived(p0)
 
+        notificationBuilder(p0?.data?.get("title")!!, p0.data?.get("body")!!,
+                p0.data?.get("date")!!, p0.data?.get("time")!!)
+
+        wakeUpScreen()
+    }
+
+    //this function to build the notification
+    private fun notificationBuilder(title: String, body: String, date: String, time: String) {
+
         // Create an explicit intent for an Activity in your app
         val notifyIntent = Intent(this, EventDetails::class.java).apply {
             flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-            putExtra("title", p0?.data?.get("title"))
-            putExtra("body", p0?.data?.get("body"))
-            putExtra("date", p0?.data?.get("date"))
-            putExtra("time", p0?.data?.get("time"))
+            putExtra("title", title)
+            putExtra("body", body)
+            putExtra("date", date)
+            putExtra("time", time)
         }
         val notifyPendingIntent = PendingIntent.getActivity(
                 this, 0, notifyIntent, PendingIntent.FLAG_UPDATE_CURRENT
@@ -31,11 +43,11 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
         val mBuilder = NotificationCompat.Builder(this,
                 resources.getString(R.string.default_notification_channel_id))
                 .setSmallIcon(R.drawable.ic_stat_name_notification_logo)
-                .setContentTitle(p0?.data?.get("title"))
-                .setContentText(p0?.data?.get("body"))
+                .setContentTitle(title)
+                .setContentText(body)
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT)
                 .setStyle(NotificationCompat.BigTextStyle()
-                        .bigText(p0?.data?.get("body")))
+                        .bigText(body))
                 .setDefaults(Notification.DEFAULT_ALL)
                 // Set the intent that will fire when the user taps the notification
                 .setContentIntent(notifyPendingIntent)
@@ -45,6 +57,23 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
             val notificationId = Random().nextInt()
             // notificationId is a unique int for each notification that you must define
             notify(notificationId, mBuilder.build())
+        }
+    }
+
+    @SuppressLint("InvalidWakeLockTag")
+    private fun wakeUpScreen() {
+        //wake up lock screen
+        val pm = applicationContext.getSystemService(Context.POWER_SERVICE)
+                as PowerManager
+        val isScreenOn = pm.isScreenOn
+        if (!isScreenOn) {
+            val wl = pm.newWakeLock(PowerManager.FULL_WAKE_LOCK
+                    or PowerManager.ACQUIRE_CAUSES_WAKEUP or PowerManager.ON_AFTER_RELEASE,
+                    "mywakelocktag")
+            wl.acquire(10000)
+            val wl_cpu = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK,
+                    "mycpulocktag")
+            wl_cpu.acquire(10000)
         }
     }
 }
